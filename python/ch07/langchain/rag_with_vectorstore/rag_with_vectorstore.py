7abb30cbe691c627e71d442c6beb9e1cf75c73b8
Boni Garcia <boni.garcia@uc3m.es>
2026-02-06 10:35:07 +0100

    [CH07] Fix imports in RAG example

--- python/ch07/langchain/rag_with_vectorstore/rag_with_vectorstore.py
+++ python/ch07/langchain/rag_with_vectorstore/rag_with_vectorstore.py
@@ -7,8 +7,8 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
 
 # Load environment variables from .env file
 load_dotenv()
