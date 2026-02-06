bf2eb9a966189c0414ff09ffe41f455ced249410
Boni Garcia <boni.garcia@uc3m.es>
2026-02-06 11:13:16 +0100

    [CH07] Set use_docker to false in AutoGen demo

--- python/ch07/autogen/rag_with_autogen/rag_with_autogen.py
+++ python/ch07/autogen/rag_with_autogen/rag_with_autogen.py
@@ -36,6 +36,7 @@ user_proxy = autogen.UserProxyAgent(
     human_input_mode="NEVER",
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={"use_docker": False},
 )
 
 # Read the external data
